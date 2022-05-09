55a70f06a8f722e0922fa194293b0fe5b818c9cb
Ingo Breßler <ingo.bressler@bam.de>
2022-05-09 18:30:29 +0200

    doc: removed obsolete module import

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,8 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-# import required modules proactively which cause sphinx errors otherwise
-import dateutil
 
 # -- Project information -----------------------------------------------------
 
