b9dcfa3074a370e54d0969a5df048151bdd692fa
Ingo Breßler <ingo.bressler@bam.de>
2022-09-21 19:03:41 +0200

    compat. for new cgs3 testdata added
    - moved tests to separate file in subdir
    - updated notebook

--- dlshelpers.py
+++ dlshelpers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # utils.py
 
-import os, re, io
+import os, re, io, codecs
 from warnings import warn
 from pathlib import Path
 from dateutil.parser import parse as parseDateTime
@@ -31,130 +31,39 @@ def parseValue(val):
         pass
     return val
 
-def getDLSFileMeta(lines, encoding='cp1250'):
-    """Reads the measurement settings (temperature, viscosity, refractive index and wavelength)
-    >>> from pprint import pprint
-    >>> with open("testdata/water/093 2021 Wasser0000_0001.ASC", encoding='cp1250') as fh:\
-            pprint(getDLSFileMeta(fh.readlines()))
-    {'Angle(1)[°]': 26.0,
-     'Angle(2)[°]': 42.0,
-     'Angle(3)[°]': 58.0,
-     'Angle(4)[°]': 74.0,
-     'Angle(5)[°]': 90.0,
-     'Angle(6)[°]': 106.0,
-     'Angle(7)[°]': 122.0,
-     'Angle(8)[°]': 138.0,
-     'Atten[1]': 1.0,
-     'Atten[2]': 1.0,
-     'Atten[3]': 1.0,
-     'Correlation': '',
-     'DC[1][kHz]': 0.0,
-     'DC[2][kHz]': 0.0,
-     'DC[3][kHz]': 0.0,
-     'DC[4][kHz]': 0.0,
-     'DC[5][kHz]': 0.0,
-     'DC[6][kHz]': 0.0,
-     'DC[7][kHz]': 0.0,
-     'DC[8][kHz]': 0.0,
-     'Date': '11.05.2021',
-     'Duration [s]': 30.0,
-     'MeanCR1 [kHz]': 3.33436,
-     'MeanCR2 [kHz]': 2.38679,
-     'MeanCR3 [kHz]': 2.06514,
-     'MeanCR4 [kHz]': 1.71528,
-     'MeanCR5 [kHz]': 1.52074,
-     'MeanCR6 [kHz]': 2.09028,
-     'MeanCR7 [kHz]': 1.73645,
-     'MeanCR8 [kHz]': 1.11897,
-     'Mode': '8 x AUTO, 100 ns STC',
-     'Refractive Index': 1.332,
-     'RelSens[1]': 1.0,
-     'RelSens[2]': 1.0,
-     'RelSens[3]': 1.0,
-     'RelSens[4]': 1.0,
-     'RelSens[5]': 1.0,
-     'RelSens[6]': 1.0,
-     'RelSens[7]': 1.0,
-     'RelSens[8]': 1.0,
-     'Runs': 1.0,
-     'SampMemo(0)': 'Wasser-Messung ',
-     'SampMemo(1)': '',
-     'SampMemo(2)': '',
-     'SampMemo(3)': '',
-     'SampMemo(4)': '',
-     'SampMemo(5)': '',
-     'SampMemo(6)': '',
-     'SampMemo(7)': '',
-     'SampMemo(8)': '',
-     'SampMemo(9)': '',
-     'Samplename': '093 2021 Wasser',
-     'Temperature [K]': 292.95295,
-     'Time': '09:35:43',
-     'Viscosity [cp]': 1.0066,
-     'Wavelength [nm]': 632.8,
-     'corrStartLn': 57,
-     'crStartLn': 240,
-     'filetype': 'ALV-7004 CGS-8F Data, Single Run Data',
-     'monitorDiode': 1562861.47,
-     'monitorDiodeLn': 496}
+def bufferFromLines(ln):
+    assert isList(ln)
+    return io.StringIO("".join(ln))
 
-    >>> with open("testdata/cgs3/080622_3_0033_0002.ASC", encoding='cp1250') as fh:\
-            pprint(getDLSFileMeta(fh.readlines()))
-    {'Angle [°]': 90.0,
-     'Correlation': '',
-     'Date': '08.06.2022',
-     'Duration [s]': 10.0,
-     'FloatDur [ms]': 9961.0,
-     'MeanCR0 [kHz]': 305.63556,
-     'MeanCR1 [kHz]': 312.20247,
-     'MeanCR2 [kHz]': 0.0,
-     'MeanCR3 [kHz]': 0.0,
-     'Mode': 'C-CH0/1+1/0',
-     'Refractive Index': 1.332,
-     'Runs': 1.0,
-     'SampMemo(0)': '',
-     'SampMemo(1)': '',
-     'SampMemo(2)': '',
-     'SampMemo(3)': '',
-     'SampMemo(4)': '',
-     'SampMemo(5)': '',
-     'SampMemo(6)': '',
-     'SampMemo(7)': '',
-     'SampMemo(8)': '',
-     'SampMemo(9)': '',
-     'Samplename': 'Samplename needed.',
-     'Stop TP [ms]': 16777216.0,
-     'Temperature [K]': 297.9244,
-     'Time': '14:31:40',
-     'Viscosity [cp]': 0.89482,
-     'Wavelength [nm]': 632.8,
-     'corrStartLn': 29,
-     'crStartLn': 230,
-     'cum1StartLn': 487,
-     'cum2StartLn': 492,
-     'cum3StartLn': 498,
-     'filetype': 'ALV-7004/USB',
-     'monitorDiode': 152207.46,
-     'monitorDiodeLn': 485,
-     'stddevStartLn': 505}
+def getDLSFileMeta(lines, encoding='cp1250'):
+    """Reads the measurement settings (temperature, viscosity, refractive index and wavelength).
+    The *MeanCR* field is ignored because they were found to be either inconsistent with the
+    recorded count rate in the same file (type `ALV-7004/USB`) or they have the wrong ordering
+    compared to the recorded count rate in the same file (type `ALV-7004 CGS-8F Data`). The
+    means can be calculated easily from the count rate read by *getDLSFileData()*.
+    **For examples**, please see the tests for this function in `tests/test_dlshelpers.py`.
     """
     assert isList(lines)
     meta = {'filetype': lines[0].strip()}
+    # section names to look for and their keys in the metadata dict
     sections = [('corrStartLn', '"Correlation"'), ('crStartLn', '"Count Rate"'),
                 ('monitorDiodeLn', 'Monitor Diode'), ('cum1StartLn', '"Cumulant 1.Order"'),
                 ('cum2StartLn', '"Cumulant 2.Order"'), ('cum3StartLn', '"Cumulant 3.Order"'),
                 ('stddevStartLn', '"StandardDeviation"')]
+    meta['sections'] = {key: -1 for key, _ in sections}
     key, pattern = sections.pop(0)
     for idx, line in enumerate(lines):
         if pattern in line:
-            meta[key] = idx
+            meta['sections'][key] = idx
             if len(sections):
                 key, pattern = sections.pop(0)
             else:
                 break
+    assert meta['sections']['corrStartLn'] > 0, \
+            'The mandatory "Correlation" section was not found!'
     # read the recorded environment data, device settings and sample notes
-    df = pd.read_csv(io.StringIO("".join(lines)), sep=':', encoding=encoding,
-                       skiprows=1, nrows=meta['corrStartLn']-1,
+    df = pd.read_csv(bufferFromLines(lines), sep=':', encoding=encoding,
+                       skiprows=1, nrows=meta['sections']['corrStartLn']-2,
                        index_col=0,
                        names=range(5), # also read fields containing two colons
                        na_filter=False, # don't replace empty fields with NaN
@@ -165,44 +74,54 @@ def getDLSFileMeta(lines, encoding='cp1250'):
                       )
     # parse floats and put datetimes back together
     meta.update({name.strip():
-                 parseValue(':'.join(field for field in df.T[name] if len(field)
-                                    ).strip())
+                 parseValue(':'.join(field for field in df.T[name] if len(field)).strip())
                  for name in df.index
+                 if "MeanCR" not in name
                 })
-    meta['monitorDiode'] = float(lines[meta['monitorDiodeLn']].split()[-1])
+    if meta['sections']['monitorDiodeLn'] > 0:
+        meta['monitorDiode'] = float(lines[meta['sections']['monitorDiodeLn']].split()[-1])
+    # return the sorted dict
     return meta
 
 def getDLSFileData(filename, showProgress=False, encoding='cp1250',
                    attenKeys={'key': 'abgeschwächt', 'detectorKey': 'detektor', 'levelKey': 'stufe'}):
     """
-    #>>> getDLSFileData("testdata/water/093 2021 Wasser0000_0001.ASC")
+    *Mode*: The mode describes which channels (count rate columns) were used for auto- or
+    cross-correlation, as given in
+    [the specs sheet by ALV](https://www.alvgmbh.de/dwnload/alv7000specs.pdf).
+    **For examples**, please see the tests for this function in `tests/test_dlshelpers.py`.
     """
     if showProgress:
         print('.', end="") # some progress output
-    data = dict(filename=Path(filename).resolve())
-    with open(data['filename'], encoding=encoding) as fh:
-        filedata = fh.readlines()
-    header = getDLSFileMeta(filedata)
-    data.update(sampleName=header["Samplename"])
-    data.update(timestamp=parseDateTime(header['Date']+' '+header['Time']))
+    filename = Path(filename)
+    filelines = []
+    with codecs.open(filename, encoding=encoding) as fh:
+        filelines = [ln for ln in fh.readlines() if len(ln.strip())] # filter empty lines
+    data = getDLSFileMeta(filelines)
+    data['filename'] = filename
+    data['timestamp'] = parseDateTime(data['Date']+' '+data['Time'])
+
     # parsing the scattering angles
-    angles = [value for key,value in header.items() if key.startswith("Angle")]
-    data.update(angles=angles)
-    # try to get the concentration from the memo field
-    memostr = "".join([value for key,value in header.items() if key.startswith("SampMemo")])
-    #print(f"memostr: '{memostr}'")
-    memofields = [field.strip(' ,;') for field in memostr.split()]
+    angles = [value for key,value in data.items() if key.startswith("Angle")]
+    data['angles'] = angles
+
+    # try to get the concentration from the memo field, TODO: write a test for this
+    data['memo'] = "".join([value for key,value in data.items() if key.startswith("SampMemo")])
+    #print(f"memostr: '{data['memo']}'")
+    data['memofields'] = [field.strip(' ,;') for field in data['memo'].split()]
     try:
-        concentration = [float(field.split(':')[1]) for field in memofields if ':' in field][0]
+        concentration = [float(field.split(':')[1])
+                         for field in data['memofields'] if ':' in field][0]
         concentration = 1/float(concentration)
     except (IndexError, ValueError):
         concentration = 1
-    data.update(concentration=concentration)
-    # parsing attenuation values
-    atten = [value for key,value in header.items() if key.startswith("Atten")]
+    data['concentration'] = concentration
+
+    # parse attenuation values
+    atten = [value for key,value in data.items() if key.startswith("Atten")]
     atten += [1.0] * (len(angles) - len(atten)) # pad attenuation list to have same amount as angles (only 3 stored in file)
-    if attenKeys['key'] in memostr:
-        attenManual = getAttenuationFromMemo(memostr, len(angles), **attenKeys)
+    if attenKeys['key'] in data['memo']:
+        attenManual = getAttenuationFromMemo(data['memo'], len(angles), **attenKeys)
         if atten != attenManual:
             warn("\n"f"  Attenuation values specified in sample description: {attenManual}\n"
                  f"  ('{memostr}'),\n"
@@ -210,38 +129,62 @@ def getDLSFileData(filename, showProgress=False, encoding='cp1250',
                  f"  -> Using values from sample description:            {attenManual}.", # 'cause they may contain 4 instead of 3 values
                  None, stacklevel=2)
             atten = attenManual
-    data.update(attenuation=pd.DataFrame(np.array(atten).reshape(1,-1), columns=angles))
-    # ATTENTION! MeanCR is stored in reversed order in these files for
-    # the acquisition software used here: "ALV MultiAngle" Version 3.1.4.3
-    meancr = np.flip(np.array([value for key,value in header.items() if key.startswith("MeanCR")]))
-    data.update(meancr=pd.DataFrame(meancr.reshape(1,-1), columns=angles))
-    for name in "Temperature", "Viscosity", "Refractive Index", "Wavelength":
-        for key,value in header.items():
-            if key.startswith(name):
-                data[key] = value
-    with open(filename, encoding='cp1250') as fd:
-        lines = fd.readlines()
-        #[print(ln.strip()) for ln in lines[17:25]]
-        value = [line for line in lines if line.startswith('Monitor Diode')]
-        if len(value): # empty for '_averaged' files
-            value = float(value[0].split()[-1])
-            data.update(monitorDiode=value)
-        #print("data", data)
-        def findMatchingLine(lineLst, pattern):
-            idx = [idx for idx, ln in enumerate(lineLst) if pattern in ln]
-            return int(idx[0]) if len(idx) else len(lineLst)-1
-        corrstart = findMatchingLine(lines, 'Correlation')
-        crstart   = findMatchingLine(lines, 'Count Rate')
-        crend     = findMatchingLine(lines, 'Monitor Diode')
-    # read tabular data after file was closed by leaving scope of 'with'
-    if corrstart > 0:
-        corr = pd.read_csv(filename, skiprows=corrstart+1, nrows=crstart-1-corrstart,
-                           sep=r'\s+', names=["tau"]+angles, index_col=0, encoding=encoding)
-        data.update(correlation=corr)
-    if (crend-2-crstart) > 0:
-        cr   = pd.read_csv(filename, skiprows=crstart+1, nrows=crend-2-crstart,
-                           sep=r'\s+', names=["time"]+angles, index_col=0, encoding=encoding)
-        data.update(countrate=cr)
+    data['attenuation'] = pd.DataFrame(np.array(atten).reshape(1,-1), columns=angles)
+
+    data = dict(sorted(data.items(), key=lambda x: x[0].lower()))
+
+    # read a section of data columns from header until begin of next section, or file end
+    lineIndices = np.array([idx for key, idx in data['sections'].items()], dtype=int)
+    def sectionEndLn(startLn): # return the index of any section directly following the given index
+        followingSections = lineIndices[lineIndices > startLn]
+        return followingSections.min() if followingSections.size else len(filelines)
+
+    # read correlation data columns (mandatory)
+    startln, endln = data['sections']['corrStartLn']+1, sectionEndLn(data['sections']['corrStartLn'])
+    corr = pd.read_csv(bufferFromLines(filelines), encoding=encoding, sep=r'\s+',
+                       index_col=0, header=None,
+                       skiprows=startln, nrows=endln-startln)
+
+    # adjust column names if more columns than angles were found
+    columns = angles
+    if len(corr.columns) > len(columns):
+        # repeat the last angle with suffix if there are more columns
+        columns = columns + ([columns[-1],] * (len(corr.columns) - len(columns)))
+        columns = [f"{int(angle)}_{idx}" if idx else angle for idx, angle in enumerate(columns)]
+    corr.rename(columns=lambda col: columns[col-1], inplace = True)
+    corr.index.names = ["tau"]
+    data['correlation'] = corr
+    
+    # read the count rate data columns
+    if data['sections']['crStartLn'] > 0: # does not exist in an 'averaged' data file
+        startln, endln = data['sections']['crStartLn']+1, sectionEndLn(data['sections']['crStartLn'])
+        cr = pd.read_csv(bufferFromLines(filelines), encoding=encoding, sep=r'\s+',
+                         names=["time"]+columns, index_col=0,
+                         skiprows=startln, nrows=endln-startln)
+        data['countrate'] = cr
+
+    # read cumulants from data
+    cumulants = []
+    for num in range(3):
+        cumulants.append(None)
+        secname = f"cum{num+1}StartLn"
+        if data['sections'][secname] <= 0:
+            continue
+        startln, endln = data['sections'][secname]+1, sectionEndLn(data['sections'][secname])
+        secname = filelines[startln-1].strip().strip('"')
+        cumulants[num] = pd.read_csv(bufferFromLines(filelines), encoding=encoding, sep=r'\t',
+                                     names=("value",), index_col=0, engine='python',
+                                     skiprows=startln, nrows=endln-startln)
+    if any([cumu is not None for cumu in cumulants]):
+        data['cumulants'] = cumulants
+
+    # read the standard deviation
+    stddevStartLn = data['sections']['stddevStartLn']
+    if stddevStartLn > 0:
+        startln, endln = stddevStartLn+1, sectionEndLn(stddevStartLn)
+        data['stddev'] = pd.read_csv(bufferFromLines(filelines), encoding=encoding, sep=r'\s+',
+                                     names=["tau", columns[0]], index_col=0,
+                                     skiprows=startln, nrows=endln-startln)
     return data
 
 def getAttenuationFromMemo(memo, count=4, detectorKey='detektor', levelKey='stufe', key=None):
